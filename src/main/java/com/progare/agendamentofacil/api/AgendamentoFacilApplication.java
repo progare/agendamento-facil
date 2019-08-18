78004a31023bae683d005ba4fb6bbd1e5a21655d
André <andre.re28@gmail.com>
2019-08-18 12:25:30 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/progare/agendamentofacil/api/AgendamentoFacilApplication.java
@@ -0,0 +1,13 @@
+package com.progare.agendamentofacil.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoFacilApplication.class, args);
+	}
+
+}
